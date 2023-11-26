4d813c228ecc77837426b30f6e9832474ca35094
cskiwi <glenn.latomme@gmail.com>
2023-11-26 13:40:13 +0100

    feat: more teseting

--- apps/gull/src/server/app.module.ts
+++ apps/gull/src/server/app.module.ts
@@ -1,17 +1,15 @@
 // needs to be first import, it loads the polyfills
-import { AngularRendererModule } from '@nitedani/angular-renderer-nestjs';
 import { Module } from '@nestjs/common';
+import { AngularRendererModule } from '@nitedani/angular-renderer-nestjs';
 import { AppComponent } from '../app/app.component';
 import { SharedModule } from '../shared.module';
-import { RouterModule } from '@angular/router';
-import { routes } from '../app/app.routes';
 
 @Module({
   imports: [
     AngularRendererModule.forRoot({
       page: AppComponent,
       // import only on server
-      imports: [SharedModule, RouterModule.forRoot(routes)],
+      imports: [SharedModule],
       // provide only on server
       providers: [],
     }),
--- apps/gull/src/shared.module.ts
+++ apps/gull/src/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
 
 // This module is imported on client, inside src/main.ts
 // This module is imported on server, inside server/app.module.ts
 // SharedModule provides AppService on client and server
 
 @NgModule({
-  imports: [],
+  imports: [RouterModule.forRoot(routes)],
   providers: [],
 })
 export class SharedModule {
