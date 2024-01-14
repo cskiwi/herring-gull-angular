3e8a68163159125b34f26af4f00094e6f6327f97
cskiwi <glenn.latomme@gmail.com>
2024-01-14 13:10:58 +0100

    not using async pipe is better

--- libs/frontend/pages/welcome/src/welcome/welcome.component.ts
+++ libs/frontend/pages/welcome/src/welcome/welcome.component.ts
@@ -5,6 +5,7 @@ import { Title } from '@angular/platform-browser';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'gull-some-component',
@@ -12,7 +13,7 @@ import { MatCardModule } from '@angular/material/card';
   imports: [CommonModule, MatCardModule, MatButtonModule],
   template: `
     <p>{{ title }}</p>
-    {{ cats$ | async | json }}
+    {{ cats() | json }}
 
     <mat-card>
       <mat-card-header>
@@ -29,8 +30,8 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class HeavyComponent implements OnInit {
   private httpClient = inject(HttpClient);
+  cats = toSignal(this.httpClient.get('/api/cats'));
 
-  cats$ = this.httpClient.get('/api/cats');
   title: string = 'Some Component started';
 
   ngOnInit(): void {
